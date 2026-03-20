6fdba02ec40d7d352ddfbc797f03e2bbc279f2d8
Michaela DeForest <michaela.deforest@gmail.com>
2026-03-20 00:26:48 -0400

    chore: rename app to Mise

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Recipe Paste',
+  title: 'Mise',
   description: 'Clip and manage recipes',
 }
 
